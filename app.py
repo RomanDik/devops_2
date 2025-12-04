5e0576df6002e779565d1cd071f0ce9f1890742f
Roman Dik <roman75d@yandex.ru>
2025-12-04 21:00:23 +0400

    docker8

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
         </head>
         <body>
             <div class="container">
-                <h1>DevOps Laboratory Work #2 88005553535</h1>
+                <h1>DevOps Laboratory Work #2 123456789</h1>
                 
                 <div class="info">
                     <p><strongStudent:</strong> {student_name}</p>
